888972ac367ff2302559d05130fee16c387421fd
limingqihust <1285057590@qq.com>
2023-12-26 23:18:03 +0800

    transform urdf to rviz

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/lmq/smartcar_description/devel:/home/lmq/catkin_ws/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/home/lmq/catkin_ws/devel/lib:/opt/ros/noetic/lib'
+export PWD='/home/lmq/smartcar_description/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/lmq/smartcar_description/devel/share/common-lisp:/home/lmq/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/lmq/smartcar_description/src:/home/lmq/catkin_ws/src:/opt/ros/noetic/share'
